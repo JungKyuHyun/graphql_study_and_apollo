000cb6442a9e4e60bca9a67bbd7aa0688542e756
정규현 (Jacob) <kh.jung@ecubelabs.com>
2019-12-08 19:21:38 +0900

    Add trivial resolver

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 const { ApolloServer } = require('apollo-server');
 
 const typeDefs = `
+    type Photo {
+        id: ID!
+        url: String!
+        name: String!
+        description: String
+    }
     type Query {
         totalPhotos: Int!
+        allPhotos: [Photo!]!
+    }
+
+    type Mutation {
+        postPhoto(name: String! description: String): Photo!
     }
 `
+let _id = 0;
+let photos = [];
 
 const resolvers = {
     Query: {
-        totalPhotos: () => 42
+        totalPhotos: () => photos.length,
+        allPhotos: () => photos
+    },
+    Mutation: {
+        postPhoto (parent, args) {
+            let newPhoto = {
+                id: _id++,
+                ...args
+            }
+            photos.push(newPhoto)
+            return newPhoto;
+        }
+    },
+    Photo: {
+        url: parent => `https://yoursite.com/img/${parent.id}.jpg`
     }
 }
 
