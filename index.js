732cdce763e516d7895e7e2e72959c4861be93e7
정규현 (Jacob) <kh.jung@ecubelabs.com>
2019-12-22 16:11:12 +0900

    Add taggedUsers-logic

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const typeDefs = `
         name: String
         avatar: String
         postedPhotos: [Photo!]!
+        inPhotos: [Photo!]!
     }
 
     enum PhotoCategory {
@@ -23,6 +24,7 @@ const typeDefs = `
         description: String
         category: PhotoCategory!
         postedBy: User!
+        taggedUsers: [User!]!
     }
 
     input PostPhotoInput {
@@ -80,6 +82,12 @@ let photos = [
         githubUser: 'sSchmidt',
     },
 ];
+let tags = [
+    {photoID: '1', userID: 'gPlake'},
+    {photoID: '2', userID: 'sSchmidt'},
+    {photoID: '2', userID: 'gPlake'},
+    {photoID: '2', userID: 'mHattrup'},
+]
 
 const resolvers = {
     Query: {
@@ -100,12 +108,20 @@ const resolvers = {
         url: parent => `https://yoursite.com/img/${parent.id}.jpg`,
         postedBy: parent => {
             return users.find(u => u.githubLogin === parent.githubUser)
-        }
+        },
+        taggedUsers: parent => tags
+        .filter(tag => tag.photoID === parent.id)
+        .map(tag => tag.userID)
+        .map(userID => users.find(u => u.githubLogin === userID))
     },
     User: {
         postedPhotos: parent => {
             return photos.filter(p => p.githubUser === parent.githubLogin)
-        }
+        },
+        inPhotos: parent => tags
+        .filter(tag => tag.userID === parent.id)
+        .map(tag => tag.photoID)
+        .map(photoID => photos.find(p => p.id === photoID))
     }
 }
 
